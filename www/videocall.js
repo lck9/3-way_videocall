f6f742bd1aca177fdcf18fc421805c95917a3a59
chakri3130 <amar.chakri426@gmail.com>
2021-06-29 02:20:46 -0800

    Update videocall.js

--- www/videocall.js
+++ www/videocall.js
@@ -3,6 +3,4 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'videocall', 'coolMethod', [arg0]);
 };
-exports.retriveData = function (arg0,arg1, success, error) {
-    exec(success, error, 'videocall', 'retriveData', [arg0,arg1]);
-};
+
