904e8618591138e8b90358ddee81eea02eeab622
Avinash <avinash.bhalki@quantummaterialscorp.com>
2021-07-08 20:52:28 -0800

    Added iOS plugin and tested using 2 numbers.

--- www/videocall.js
+++ www/videocall.js
@@ -3,4 +3,8 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'videocall', 'coolMethod', [arg0]);
 };
+exports.add = function (param1,param2, success, error) {
+    exec(success, error, 'videocall', 'add', [param1,param2]);
+};
+
 
